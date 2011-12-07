1c7cc133c320704e21fa0dc3b3d65d1df65daba5
Tim Newsham <tim.newsham@gmail.com>
2011-12-07 08:40:25 -1000

    create initial android app project template

---
+++ android/src/com/isecpartners/CarrierIQTester/CarrierIQTesterActivity.java
@@ -0,0 +1,13 @@
+package com.isecpartners.CarrierIQTester;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CarrierIQTesterActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
